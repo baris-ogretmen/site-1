8de59de50d5131f115b0c82f534b5a3bc3054d2b
baris-ogretmen <w25fxfccpb@privaterelay.appleid.com>
2025-12-29 19:52:20 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,14 @@
-apiKey : "AIzaSyAqeO-yQyQPnLsOqKmSNvVH23WPvneIjaU" , 
-  authDomain : "shaped-totem-481514-c5.firebaseapp.com" , 
-  projeId : "şekilli-totem-481514-c5" , 
-  storageBucket : "shaped-totem-481514-c5.firebasestorage.app" , 
-  messagingSenderId : "519295049418" , 
-  appId : "1:519295049418:web:7d04e18c4ea36bc088de33" , 
-  ölçümId : "G-10ENZQDXK1"
+const firebaseConfig = {
+  apiKey: "AIzaSyAqeO-yQyQPnLsOqKmSNvVH23WPvneIjaU",
+  authDomain: "shaped-totem-481514-c5.firebaseapp.com",
+  projectId: "shaped-totem-481514-c5",
+  storageBucket: "shaped-totem-481514-c5.firebasestorage.app",
+  messagingSenderId: "519295049418",
+  appId: "1:519295049418:web:7d04e18c4ea36bc088de33",
+  measurementId: "G-10ENZQDXX1"
+};
+
+// BU KISIM ÇOK ÖNEMLİ, MUTLAKA EKLE:
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
